3020f923952c196a37468b07316beef0266c4f86
jmigsan <43961001+jmigsan@users.noreply.github.com>
2022-08-07 15:57:04 +0100

    no more 'no null'

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,13 +1,19 @@
 import { Button } from '@chakra-ui/react';
 import { getProviders, signIn } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 const SignIn: React.FC<{ providers: any }> = ({ providers }) => {
+  const router = useRouter();
+
   let redirectUrl = 'https://learning-nextauth.vercel.app/';
 
   useEffect(() => {
     const url = new URL(location.href);
-    redirectUrl = url.searchParams.get('callbackUrl')!;
+    redirectUrl =
+      url.searchParams.get('callbackUrl') !== null
+        ? redirectUrl
+        : 'https://learning-nextauth.vercel.app/';
   });
 
   return (
